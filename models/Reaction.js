7ebc6711a7884d6cc56585c39a2368f6483398b6
MA-FS <87353040+MA-FS@users.noreply.github.com>
2022-01-17 02:52:44 +0800

    Reaction  model created

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+// Require Mongoose & Moment
+const { Schema, Types } = require("mongoose");
+const moment = require("moment");
+
+// Create new reactionSchema using Mongoose constructor
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => moment(timestamp).format("MMM Do, YYYY [at] hh:mm a"), // Use moment for format the timestamp on query
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
